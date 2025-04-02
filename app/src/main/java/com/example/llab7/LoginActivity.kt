56b21fb60a260c58ca43c099b39ee6dbab5239c1
moldir <moldirnursapa@gmail.com>
2025-04-02 14:40:38 +0500

    Добавила LoginActivity с обработкой логина

---
+++ app/src/main/java/com/example/llab7/LoginActivity.kt
@@ -0,0 +1,50 @@
+package com.example.llab7
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var edUsername: EditText
+    private lateinit var edPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+
+    private val CREDENTIAL_SHARED_PREF = "our_shared_pref"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        edUsername = findViewById(R.id.ed_username)
+        edPassword = findViewById(R.id.ed_password)
+        btnLogin = findViewById(R.id.btn_login)
+        btnSignUp = findViewById(R.id.btn_signup)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val credentials: SharedPreferences = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE)
+            val strUsername = credentials.getString("Username", null)
+            val strPassword = credentials.getString("Password", null)
+
+            val usernameFromEd = edUsername.text.toString()
+            val passwordFromEd = edPassword.text.toString()
+
+            if (strUsername == usernameFromEd && strPassword == passwordFromEd) {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
